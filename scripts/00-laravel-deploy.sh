e091246b1d8a73fd52d4c0fc862dcae051dd0f18
codebykenth <kenthosila@gmail.com>
2026-05-30 15:29:12 +0800

    fix: add view caching to deployment scripts to fix 500 error

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -7,5 +7,8 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Caching views..."
+php artisan view:cache
+
 echo "Running migrations..."
 php artisan migrate --force
--- start.sh
+++ start.sh
@@ -9,5 +9,8 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Caching views..."
+php artisan view:cache
+
 echo "Running migrations..."
 php artisan migrate --force
